164b48ecefc19efd47f200b1b41c8c1556d5df9a
tree <643755984@qq.com>
2022-01-14 09:44:54 +0800

    添加环境配置

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
-const baseService = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:7001' : ''
-const baseUrl = process.env.NODE_ENV === 'development' ? '/api/v1' : '/api/v1'
-
 const service = axios.create({
-    // process.env.NODE_ENV === 'development' 来判断是否开发环境
-    // easy-mock服务挂了，暂时不使用了
-    baseURL: baseService + baseUrl,
+    baseURL: import.meta.env.VITE_BASE_API + import.meta.env.VITE_BASE_URL,
     timeout: 5000
 });
 
